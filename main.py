0d7c14fc51a410e633b200e01d536625c7bee7e8
Frankie <106608697+FrankieeeTom@users.noreply.github.com>
2025-04-23 11:13:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyray import *
 from raylib import *
+from time import *
 
 WIN_WIDTH = 2000
 WIN_HEIGHT = 1000
@@ -9,6 +10,7 @@ ROAD_VEL = 500
 
 GRAVITY = 500
 JUMP_VEL = -500
+LEVEL_POS = 0
 
 class Scene:
     def __init__(self, velocity, position, texture):
@@ -18,9 +20,7 @@ class Scene:
         self.offset = 0
 
     def draw(self, frame_time):
-
         self.offset = (self.offset + (self.velocity.x * frame_time)) % self.texture.width
-
         draw_texture_pro(
             self.texture,
             Rectangle(self.offset, 0, WIN_WIDTH, self.texture.height),
@@ -30,95 +30,206 @@ class Scene:
             WHITE
         )
 
+class Collision:
+    def __init__(self, velocity, position, texture, width=300, height=30):
+        self.velocity = velocity
+        self.position = position
+        self.texture = texture
+        self.width = width
+        self.height = height
+
+    def update(self, frame_time):
+        self.position.x -= self.velocity.x * frame_time
+
+    def draw(self):
+        draw_texture_pro(
+            self.texture,
+            Rectangle(0, 0, self.width, self.height),
+            Rectangle(self.position.x, self.position.y, self.width, self.height),
+            Vector2(0, 0),
+            0,
+            WHITE
+        )
+
+    def get_rect(self):
+        return Rectangle(self.position.x, self.position.y, self.width, self.height)
+
+
+# Init
 set_config_flags(FLAG_VSYNC_HINT)
 init_window(WIN_WIDTH, WIN_HEIGHT, "Dino")
-set_target_fps(30)
-#set_window_state(FLAG_FULLSCREEN_MODE)
+set_target_fps(60)
 
 background_texture = load_texture("landscape.jpg")
 road_texture = load_texture("road_ref2.jpg")
+collision_texture = load_texture("kolize.png")
+collision2_texture = load_texture("kolize2.png")
 
 player_left_texture = load_texture("player_left_ref.png")
 player_right_texture = load_texture("player_right_ref.png")
 player_idle_texture = load_texture("player_idle_ref.png")
 
 background = Scene(Vector2(BACKGROUND_VEL, 0), Vector2(0, 0), background_texture)
-
 road = Scene(Vector2(ROAD_VEL, 0), Vector2(0, 750), road_texture)
 
-
-
-player = Vector2(950, 1000)  # Player position
-player_vel = 0  # Player velocity (initially at rest)
-player_acc = 0  # Player acceleration (gravity)
-is_jumping = False
-
+platforms = [
+    Collision(Vector2(ROAD_VEL, 0), Vector2(600, 600), collision_texture),
+    Collision(Vector2(ROAD_VEL, 0), Vector2(1200, 400), collision_texture),
+    Collision(Vector2(ROAD_VEL, 0), Vector2(1700, 500), collision_texture),
+    Collision(Vector2(ROAD_VEL, 0), Vector2(2300, 325), collision_texture),
+    Collision(Vector2(ROAD_VEL, 0), Vector2(2700, 450), collision_texture),
+]
 
 player = {
     "x": 850,
     "y": 525,
+    "width": 100,
+    "height": 210,
     "texture": player_idle_texture
 }
 
+player_vel = 0
+is_jumping = False
 
-# main game loop
+frame_delay = 0.5
+last_frame_time = time()
+frames = []
+current_frame = 0
+
+# Main loop
 while not window_should_close():
-    
     frame_time = get_frame_time()
 
     if is_key_pressed(KEY_F11):
         toggle_fullscreen()
 
+    # Hlavní logika LEVEL_POS
+    if LEVEL_POS >= 0:
+        if is_key_down(KEY_RIGHT):
+            background.velocity.x = BACKGROUND_VEL
+            road.velocity.x = ROAD_VEL
+            for p in platforms:
+                p.velocity.x = ROAD_VEL
 
-    # backgorund, road and objects movement
-    if is_key_down(KEY_RIGHT):
-        background.velocity.x = BACKGROUND_VEL
-        road.velocity.x = ROAD_VEL
-        player["texture"] = player_right_texture
+            player["texture"] = player_right_texture
+            LEVEL_POS += 5
 
-    elif is_key_down(KEY_LEFT):
-        background.velocity.x = - BACKGROUND_VEL
-        road.velocity.x = - ROAD_VEL
-        player["texture"] = player_left_texture
-    else:
-        # If no keys are pressed, stop the background from moving
-        background.velocity.x = 0
+        elif is_key_down(KEY_LEFT):
+            background.velocity.x = -BACKGROUND_VEL
+            road.velocity.x = -ROAD_VEL
+            for p in platforms:
+                p.velocity.x = -ROAD_VEL
+
+            player["texture"] = player_left_texture
+            LEVEL_POS -= 5
+
+        else:
+            background.velocity.x = 0
+            road.velocity.x = 0
+            for p in platforms:
+                p.velocity.x = 0
+
+            player["texture"] = player_idle_texture
+
+    if LEVEL_POS <= 0:
+        if LEVEL_POS <= -150:
+            player["texture"] = player_idle_texture
+            LEVEL_POS = -150
+            player["x"] = 50
+
         road.velocity.x = 0
-        player["texture"] = player_idle_texture
+        background.velocity.x = 0
+        for p in platforms:
+            p.velocity.x = 0
 
-    # Jump logic
-    if is_key_pressed(KEY_UP) and player["y"] == 525:  # Allow jump only if player is on the ground
+        if is_key_down(KEY_RIGHT):
+            player["x"] += 25
+            player["texture"] = player_right_texture
+            LEVEL_POS += 5
+
+        elif is_key_down(KEY_LEFT):
+            player["x"] -= 25
+            player["texture"] = player_left_texture
+            LEVEL_POS -= 5
+
+        else:
+            player["texture"] = player_idle_texture
+
+    # Platform movement
+    for p in platforms:
+        p.update(frame_time)
+
+    # Animace (volitelná)
+    if not is_key_down(KEY_LEFT) and not is_key_down(KEY_RIGHT):
+        if get_time() - last_frame_time >= frame_delay:
+            current_frame = (current_frame + 1) % len(frames) if frames else 0
+            last_frame_time = get_time()
+
+    # Skok
+    if is_key_pressed(KEY_UP) and not is_jumping:
         player_vel = JUMP_VEL
         is_jumping = True
 
-    if is_jumping:
-        # Apply gravity to player velocity
-        player_vel += GRAVITY * frame_time
-        player["y"] += player_vel * frame_time
+    # Gravitace
+    player_vel += GRAVITY * frame_time
+    player["y"] += player_vel * frame_time
 
-    # Prevent player from going below the ground
-    if player["y"] >= 525:
+    # Kolize s plošinami
+    player_rect = Rectangle(player["x"], player["y"], player["width"], player["height"])
+    on_platform = False
+
+    
+    for plat in platforms:
+        plat_rect = plat.get_rect()
+
+        # Kontrola pouze pokud hráč padá
+        if player_vel >= 0:
+            # Hrany
+            player_bottom = player["y"] + player["height"]
+            platform_top = plat_rect.y
+
+            # Horizontální překrytí
+            if (player["x"] + player["width"] > plat_rect.x and
+                player["x"] < plat_rect.x + plat_rect.width):
+
+                # Je dostatečně blízko shora
+                if abs(player_bottom - platform_top) <= 20:
+                    player["y"] = platform_top - player["height"]
+                    player_vel = 0
+                    is_jumping = False
+                    on_platform = True
+                    break
+
+
+    # Zem fallback
+    if player["y"] >= 525 and not on_platform:
         player["y"] = 525
         player_vel = 0
         is_jumping = False
 
-    # Start drawing
+    # Kreslení
     begin_drawing()
     clear_background(WHITE)
     draw_fps(10, 10)
-    
-    # Draw the moving background and clouds
+
+
     background.draw(frame_time)
     road.draw(frame_time)
 
-    # Draw the player
-    draw_texture(player["texture"], int(player["x"]), int(player["y"]), WHITE)
+    for p in platforms:
+        p.draw()
 
-    print(player["y"])
+    draw_texture(player["texture"], int(player["x"]), int(player["y"]), WHITE)
+    
+    draw_text(str(LEVEL_POS), 20, 20, 65, WHITE)
     
     end_drawing()
 
-# Clean up and close
+# Cleanup
 unload_texture(background_texture)
 unload_texture(road_texture)
+unload_texture(collision_texture)
+unload_texture(player_left_texture)
+unload_texture(player_right_texture)
+unload_texture(player_idle_texture)
 close_window()
